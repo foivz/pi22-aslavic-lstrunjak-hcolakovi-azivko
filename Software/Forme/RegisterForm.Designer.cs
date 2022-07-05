a6210b34d5cdac697d10be0f95a4d1de294d8fcc
HasanColakovic99 <hasancolakovic32@gmail.com>
2022-07-05 14:11:32 +0200

    Dodana logika za registraciju korisnika.

--- Software/Forme/RegisterForm.Designer.cs
+++ Software/Forme/RegisterForm.Designer.cs
@@ -34,17 +34,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.registirajSeButton = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
             this.prijaviSeButton = new System.Windows.Forms.Button();
             this.nazadButton = new System.Windows.Forms.Button();
+            this.imeTextBox = new System.Windows.Forms.TextBox();
+            this.prezimeTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.korisnickoImeTextBox = new System.Windows.Forms.TextBox();
+            this.lozinkaTextBox = new System.Windows.Forms.TextBox();
+            this.brojMobitelaTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -107,49 +107,6 @@
             this.label6.TabIndex = 5;
             this.label6.Text = "Broj mobitela:";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(239, 105);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(274, 28);
-            this.textBox1.TabIndex = 6;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(239, 155);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(274, 28);
-            this.textBox2.TabIndex = 7;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(239, 262);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(274, 28);
-            this.textBox3.TabIndex = 8;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(239, 208);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(274, 28);
-            this.textBox4.TabIndex = 9;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(239, 317);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(274, 28);
-            this.textBox5.TabIndex = 10;
-            this.textBox5.UseSystemPasswordChar = true;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(239, 374);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(274, 28);
-            this.textBox6.TabIndex = 11;
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -170,6 +127,7 @@
             this.registirajSeButton.TabIndex = 13;
             this.registirajSeButton.Text = "Registriraj se";
             this.registirajSeButton.UseVisualStyleBackColor = true;
+            this.registirajSeButton.Click += new System.EventHandler(this.registirajSeButton_Click);
             // 
             // label8
             // 
@@ -204,23 +162,66 @@
             this.nazadButton.UseVisualStyleBackColor = true;
             this.nazadButton.Click += new System.EventHandler(this.nazadButton_Click);
             // 
+            // imeTextBox
+            // 
+            this.imeTextBox.Location = new System.Drawing.Point(239, 105);
+            this.imeTextBox.Name = "imeTextBox";
+            this.imeTextBox.Size = new System.Drawing.Size(274, 28);
+            this.imeTextBox.TabIndex = 17;
+            // 
+            // prezimeTextBox
+            // 
+            this.prezimeTextBox.Location = new System.Drawing.Point(239, 156);
+            this.prezimeTextBox.Name = "prezimeTextBox";
+            this.prezimeTextBox.Size = new System.Drawing.Size(274, 28);
+            this.prezimeTextBox.TabIndex = 18;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(239, 213);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(274, 28);
+            this.emailTextBox.TabIndex = 19;
+            // 
+            // korisnickoImeTextBox
+            // 
+            this.korisnickoImeTextBox.Location = new System.Drawing.Point(239, 263);
+            this.korisnickoImeTextBox.Name = "korisnickoImeTextBox";
+            this.korisnickoImeTextBox.Size = new System.Drawing.Size(274, 28);
+            this.korisnickoImeTextBox.TabIndex = 20;
+            // 
+            // lozinkaTextBox
+            // 
+            this.lozinkaTextBox.Location = new System.Drawing.Point(239, 322);
+            this.lozinkaTextBox.Name = "lozinkaTextBox";
+            this.lozinkaTextBox.Size = new System.Drawing.Size(274, 28);
+            this.lozinkaTextBox.TabIndex = 21;
+            this.lozinkaTextBox.UseSystemPasswordChar = true;
+            // 
+            // brojMobitelaTextBox
+            // 
+            this.brojMobitelaTextBox.Location = new System.Drawing.Point(239, 379);
+            this.brojMobitelaTextBox.Name = "brojMobitelaTextBox";
+            this.brojMobitelaTextBox.Size = new System.Drawing.Size(274, 28);
+            this.brojMobitelaTextBox.TabIndex = 22;
+            // 
             // RegisterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(588, 596);
+            this.Controls.Add(this.brojMobitelaTextBox);
+            this.Controls.Add(this.lozinkaTextBox);
+            this.Controls.Add(this.korisnickoImeTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.prezimeTextBox);
+            this.Controls.Add(this.imeTextBox);
             this.Controls.Add(this.nazadButton);
             this.Controls.Add(this.prijaviSeButton);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.registirajSeButton);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -246,16 +247,16 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button registirajSeButton;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button prijaviSeButton;
         private System.Windows.Forms.Button nazadButton;
+        private System.Windows.Forms.TextBox imeTextBox;
+        private System.Windows.Forms.TextBox prezimeTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox korisnickoImeTextBox;
+        private System.Windows.Forms.TextBox lozinkaTextBox;
+        private System.Windows.Forms.TextBox brojMobitelaTextBox;
     }
 }
